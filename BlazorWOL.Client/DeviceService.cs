a972f74991914ac617ae23beb5b7ece52b17e5b3
Hiroto Takahashi <hoakinfx@yahoo.co.jp>
2018-08-05 19:04:00 +0900

    test commit

--- BlazorWOL.Client/DeviceService.cs
+++ BlazorWOL.Client/DeviceService.cs
@@ -12,7 +12,7 @@ namespace BlazorWOL.Client
         {
             new Device {Name = "Odin", MACAddress = "00:15:5D:52:CA:B6" },
             new Device {Name = "Thor", MACAddress = "00:0C:29:30:7D:5D" },
-            new Device {Name = "Fenrir", MACAddress = "00:50:56:01:43:86" }
+            new Device {Name = "Fenrir", MACAddress = "00:50:56:01:43:85" }
         };
 
         public async Task<IEnumerable<Device>> GetServicesAsync()
